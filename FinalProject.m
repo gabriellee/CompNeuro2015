31b2a6afde60fdafba1eaa8f461d58be91ba1a75
gabriellee <gewall@B77YLX1.olin.edu>
2015-04-28 15:41:45 -0400

    added weight calculation

--- FinalProject.m
+++ FinalProject.m
@@ -8,14 +8,18 @@
 t_end = 100;%ms
 dt =.1;
 tau_e = 20%ms
+tau_weights = 20;
 w_post_pre = zeros(1,t_end);%weight of the presynaptic cell onto the postsynaptic cell
 w_post_pre(1) = .5;
-stim_in = .5;%strength of input from retinal ganglion cells
+stim_in = .5;%c_c: strength of input from retinal ganglion cells
 t_vec = 0:dt:t_end;
 r_ex =zeros(1,length(t_end))
 r_mean = 5;%hz
+A_plus = 0.003;%magnitude of LTP
+A_minus = .003/.99;%magnitude of LTD
+delta_w = 0;
 
-%define temporal change in PSPs
+%define temporal change in PSPs (epsilon)
 %generate poisson spike distribution using the mean rate to determine when
 %the next spike will occur
 spike_rate_vec1 = poissrnd(r_mean, 1, t_end*dt);
@@ -26,20 +30,32 @@ for i=1:length(spike_rate_vec1)
     end
 end
 
+%finding rates
 for i=1:t_end*dt
     temp_sum = 0;
     t = t_vec(i);
+    %changing the value of PSP_var (E)
     if t>=0
         tau_e = 20%ms
         PSP_var(i) = (t/tau_e^2)*e^(-t/tau_e);
+        
     else
         PSP_var(i) = 0;
+    end
+    %changing weights
+    if t > 0:
+        delta_w = A_plus * exp(-dt/tau_weights);
+    elseif t < 0:
+        delta_w = -A_minus * exp(dt/tau_weights);
+    else:
+        delta_w = 0;
     end
     
     for f = 0:sum(spike_vec1(i))
         temp_sum = temp_sum + PSP_var(i - (1/spike_rate_vec1(i))/dt) ...
             + r_ex_base;
     end
+    
     r_ex(i) = temp_sum * stim_in;
 end
     
